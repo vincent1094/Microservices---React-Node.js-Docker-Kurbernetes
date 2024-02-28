71dbcd1ad7290f7d19a291c240697cd22e4510c1
Thanh Nguyen <joseph_nguyen1094@yahoo.com>
2024-02-28 16:58:58 +0700

    Completed React App

--- blog-boilerplate/client/src/components/PostList.jsx
+++ blog-boilerplate/client/src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 function PostList() {
     const [posts, setPosts] = useState({});
@@ -14,9 +16,25 @@ function PostList() {
         fetchPosts();
     }, []); 
 
-    console.log(posts);
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+        <div 
+            className="card"
+            style={{ width: '30%', marginBottom: '20px' }}
+            key={post.id}
+        >
+            <div className="card-body">
+                <h3>{post.title}</h3>
+                <CommentList postId={post.id}/>
+                <CommentCreate postId={post.id}/>
+            </div>
+        </div>
+        );
+    });
     
-    return <div></div>
+    return <div className="d-flex flex-row flex-wrap justify-content-between">
+        {renderedPosts}
+    </div>;
 }
 
 export default PostList;
